a9a165087ee7446e224d48fc79f702a882b40de8
chicken-1 <nguyenthanhan3703@gmai.com>
2024-08-08 09:00:48 +0700

    update convention

--- Assg2_CMSIS/CMSIS_DRIVER/Driver_GPIO.c
+++ Assg2_CMSIS/CMSIS_DRIVER/Driver_GPIO.c
@@ -14,14 +14,15 @@
 * Functions
 *******************************************************************************/
 void Driver_GPIO_InitLed() {
-    PCC->CLKCFG[PCC_PORTB_INDEX] |= 1 << 30;
-    PORTB->PCR[4] |= 1 << 8;
-    FGPIOB->PDDR |= 1 << 4;
-    FGPIOB->PDOR |= 1 << 4;
+    PCC->CLKCFG[PCC_PORTB_INDEX] |= 1 << 30;	/* Enable clock for Port B */
+    PORTB->PCR[4] |= 1 << 8;			/* Configure PTB4 as a GPIO pin */
+    FGPIOB->PDDR |= GREEN_LED_PIN ;		/* Set PTB4 as an output */
+    FGPIOB->PDOR |= GREEN_LED_PIN ;		/* Turn led off */
 }
 
 void Driver_GPIO_Delay200us() {
     uint32_t index;
+
     for(index = 0; index < DELAY_CNT_200us; index++) {
         __asm("nop");
     }
@@ -29,6 +30,7 @@ void Driver_GPIO_Delay200us() {
 
 void Driver_GPIO_Delay1s() {
     uint32_t index;
+
     for(index = 0; index < DELAY_CNT_1s; index++) {
         __asm("nop");
     }
@@ -36,18 +38,19 @@ void Driver_GPIO_Delay1s() {
 
 void Driver_GPIO_DutyCycle2s(uint32_t duty) {
     uint32_t index;
-    FGPIOB->PTOR |= 1 << 4;
+
+    Driver_GPIO_Toggle();
     for(index = 1; index <= duty; index++) {
         Driver_GPIO_Delay200us();
     }
-    FGPIOB->PTOR = 1 << 4;
+    Driver_GPIO_Toggle();
     for(index = 1; index <= 100 - duty; index++) {
         Driver_GPIO_Delay200us();
     }
 }
 
 void Driver_GPIO_Toggle() {
-    FGPIOB->PTOR |= GREEN_LED_PIN;
+    FGPIOB->PTOR |= GREEN_LED_PIN;	/* Tonggle the LED */
 }
 
 /*******************************************************************************
