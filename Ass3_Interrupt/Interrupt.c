419a9965f258ceed32ff8186286e00d4aa5e2bea
tkstung104 <tungha0403@gmail.com>
2024-08-14 08:20:46 +0700

    Update main

--- Ass3_Interrupt/Interrupt.c
+++ Ass3_Interrupt/Interrupt.c
@@ -9,7 +9,8 @@
 #define PCR_MUX_GPIO_Mask		1 << 8
 #define PCC_CGC_Mask			1 << 30
 #define GREEN_LED_PIN			4
-#define MODE2_DURATION_TICKS	9600000 /* 200ms = 9600000 ticks */
+#define BUTTON_PIN				2
+#define MODE2_DURATION_TICKS	4800000 /* 100ms = 4800000 ticks */
 
 static uint32_t duty_cycle = 1;
 static uint32_t duty = 1;
@@ -29,11 +30,11 @@ void Init_GREEN_LED() {
 void InitSW3() {
 	PCC->CLKCFG[PCC_PORTD_INDEX] |= PCC_CGC_Mask;	/* Enable clock for Port D */
 
-	PORTD->PCR[2] |= PCR_MUX_GPIO_Mask;				/* Configure PTD2 as a GPIO pin */
-	GPIOD->PDDR &= ~(1 << 2);						/* Set PTD2 as an input */
-	PORTD->PCR[2] |= PORT_PCR_IRQC(10);				/* Configure IRQ - falling edge */
+	PORTD->PCR[2] |= PCR_MUX_GPIO_Mask;	/* Configure PTD2 as a GPIO pin */
+	FGPIOD->PDDR &= ~(1 << BUTTON_PIN);				/* Set PTD2 as an input */
+	PORTD->PCR[2] |= PORT_PCR_IRQC(10);	/* Configure IRQ - falling edge */
 
-	PORTD->PCR[2] |= 1 << 24; 						/* Delete Interrupt flag status */
+	PORTD->PCR[2] |= 1 << 24; 				/* Delete Interrupt flag status */
 	__NVIC_EnableIRQ(PORTBCD_IRQn);					/* Enable Interrupt - PORT BCD */
 }
 
@@ -47,17 +48,17 @@ void mode2() {
 		ticks_mode2 = 0;
 	}
 	else {
-		ticks_mode2 += 19200; /* 19200 ticks = 400us */
+		ticks_mode2 += 19200; 	/* 19200 ticks = 400us */
 	}
 }
 
 void mode3() {
 	if(duty <= duty_cycle) {
-		FGPIOB->PDOR &= ~(1 << 4);
+		FGPIOB->PDOR &= ~(1 << GREEN_LED_PIN);
 		duty++;
 	}
 	else {
-		FGPIOB->PDOR |= 1 << 4;
+		FGPIOB->PDOR |= 1 << GREEN_LED_PIN;
 		duty++;
 	}
 
@@ -65,8 +66,15 @@ void mode3() {
 		duty = 1;
 		duty_cycle++;
 	}
+	else {
+		/* Do nothing */
+	}
+	
 	if(duty_cycle == 101) {
 		duty_cycle = 1;
+	}
+	else {
+		/* Do nothing */
 	}
 }
 
--- Ass3_Interrupt/Main.c
+++ Ass3_Interrupt/Main.c
@@ -6,11 +6,17 @@
 /*******************************************************************************
 * Definitions
 *******************************************************************************/
+#define TRUE 1
+#define FALSE 0
+
 static mode current_mode = STOP_MODE;
+static uint8_t mode_change_flag = FALSE;
 
 /*******************************************************************************
 * Prototypes
 *******************************************************************************/
+uint32_t SysTick_Config_User(uint32_t ticks);
+
 void PORTBCD_IRQHandler(void);
 
 void SysTick_Handler(void);
@@ -21,33 +27,48 @@ void SysTick_Handler(void);
 int main() {
 	Init_GREEN_LED();
 	InitSW3();
-	SysTick_Config(19200); /* 19200 ticks = 400us */
+	SysTick_Config_User(19200); /* 19200 ticks = 400us */
 
 	while(1) {
+		/* Check mode flag	 */
+		if(mode_change_flag) {
+			/* Delete flag */
+			mode_change_flag = FALSE;
 
+			switch(current_mode) {
+			case STOP_MODE:
+				current_mode = MODE1;
+				break;
+			case MODE1:
+				current_mode = MODE2;
+				break;
+			case MODE2:
+				current_mode = MODE3;
+				break;
+			case MODE3:
+				current_mode = STOP_MODE;
+				break;
+			default:
+				break;
+			}
+		}
 	}
 }
 
 /*******************************************************************************
 * Functions
 *******************************************************************************/
-void PORTBCD_IRQHandler() {
-	// Clear ISF flag
-	PORTD->PCR[2] |= 1 << 24;
+uint32_t SysTick_Config_User(uint32_t ticks) {
+	SysTick->LOAD = ticks - 1;
+	SysTick->VAL = 0;
+	SysTick->CTRL = (1 << 2) | (1 << 1) | ( 1 << 0);
+}
 
-	// mode conversion
-	if (current_mode == STOP_MODE) {
-		current_mode = MODE1;
-	}
-	else if (current_mode == MODE1) {
-		current_mode = MODE2;
-	}
-	else if (current_mode == MODE2) {
-		current_mode = MODE3;
-	}
-	else {
-		current_mode = STOP_MODE;
-	}
+void PORTBCD_IRQHandler() {
+	/* Clear ISF flag */
+	PORTD->PCR[2] |= 1 << 24;
+	/* Set flag to indicate mode change */
+	mode_change_flag = TRUE;
 }
 
 void SysTick_Handler() {
