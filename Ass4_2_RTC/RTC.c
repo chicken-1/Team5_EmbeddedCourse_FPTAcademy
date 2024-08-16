d6386343cb0c17769ca695d95e83a97a1a417dc9
chicken-1 <nguyenthanhan3703@gmai.com>
2024-08-16 16:44:38 +0700

    update RTC.c

--- Ass4_2_RTC/RTC.c
+++ Ass4_2_RTC/RTC.c
@@ -4,10 +4,10 @@
 #define BLUE_LED_PIN			1
 #define PCR_MUX_GPIO_Mask		1 << 8
 
-unsigned char led_green_on;
-unsigned char led_green_off;
-unsigned char led_blue_on;
-unsigned char led_blue_off;
+unsigned char led_green_on = 1;
+unsigned char led_green_off = 2;
+unsigned char led_blue_on = 2;
+unsigned char led_blue_off = 1;
 
 void initLED() {
 	PCC->CLKCFG[PCC_PORTB_INDEX] |= PCC_CLKCFG_CGC_MASK;
@@ -21,9 +21,9 @@ void initLED() {
 	FGPIOB->PDDR |= 1 << GREEN_LED_PIN;
 	FGPIOD->PDDR |= 1 << BLUE_LED_PIN;
 
-	/* Configure off for LED green & blue */
-	FGPIOB->PDOR |= 1 << GREEN_LED_PIN;
-	FGPIOD->PDOR |= 1 << BLUE_LED_PIN;
+	/* Configure on for LED green & blue */
+	FGPIOB->PDOR &= ~(1 << GREEN_LED_PIN);
+	FGPIOD->PDOR &= ~(1 << BLUE_LED_PIN);
 }
 
 void initRTC() {
@@ -43,25 +43,42 @@ void initRTC() {
 
 	/* Disable interrupts */
 	RTC->IER = 0;
-	/* Stop timer */
-	RTC->TSR = 0;
+
+	/* Enable time counter */
 	RTC->SR |= RTC_SR_TCE_MASK;
 }
 
-int main() {
-	uint32_t previous_time = 0;
-	uint32_t current_time = 0;
 
+int main() {
 	initLED();
 	initRTC();
+	uint32_t previous_time_green = RTC->TSR;
+	uint32_t previous_time_blue = RTC->TSR;
+	uint32_t current_time = RTC->TSR;
+	uint8_t green_on = 1;	/* state of LED green*/
+	uint8_t blue_on = 1;	/* state of LED blue */
 
-	previous_time = RTC->TSR;
-	current_time = RTC->TSR;
 	while(1) {
-		/* 5000? not for sure (try to delay 5s for green led)*/
-		if ((current_time - previous_time) >= 1) {
-			previous_time = current_time;
-			FGPIOB->PTOR |= 1 << GREEN_LED_PIN;
+		/* blinking LED green */
+		if (((current_time - previous_time_green) >= led_green_off) && (!green_on)) {
+			previous_time_green = current_time;
+			FGPIOB->PDOR &= ~(1 << GREEN_LED_PIN); 	// turn on LED green
+			green_on = 1;
+		}
+		else if (((current_time - previous_time_blue) >= led_blue_off) && (!blue_on)) {
+			previous_time_blue = current_time;
+			FGPIOB->PDOR &= ~(1 << BLUE_LED_PIN);;	// turn on LED blue
+			blue_on = 1;
+		}
+		else if (((current_time - previous_time_green) >= led_green_on) && (green_on)) {
+			previous_time_green = current_time;
+			FGPIOB->PDOR |= 1 << GREEN_LED_PIN;		// turn off LED green
+			green_on = 0;
+		}
+		else if(((current_time - previous_time_blue) >= led_blue_on) && (blue_on)) {
+			previous_time_blue = current_time;
+			FGPIOB->PDOR |= 1 << BLUE_LED_PIN;		// turn off LED blue
+			blue_on = 0;
 		}
 		else {
 			current_time = RTC->TSR;
