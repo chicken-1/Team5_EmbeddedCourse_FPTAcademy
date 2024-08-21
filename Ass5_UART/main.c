c2e06a0c25141f9ac06d71a25bcd9208d19a9827
FuongTran <137101026+FuongTran@users.noreply.github.com>
2024-08-21 15:42:00 +0700

    Update main.c

--- Ass5_UART/main.c
+++ Ass5_UART/main.c
@@ -1,7 +1,51 @@
+/*******************************************************************************
+* Includes
+*******************************************************************************/
 #include "uart.h"
 
+/*******************************************************************************
+* Variables
+*******************************************************************************/
 uint8_t countSystick = 0; 	/* Count ticks in 1 second */
 
+/*******************************************************************************
+* Prototypes
+*******************************************************************************/
+
+/**
+ * @brief SysTick interrupt handler.
+ *
+ * This function handles the SysTick timer interrupt. It increments the
+ * `countSystick` variable every 250ms. When the counter reaches 4,
+ * it sends a message over UART0 and resets the counter.
+*/
+void SysTick_Handler();
+
+/**
+ * @brief UART0 interrupt handler.
+ *
+ * This function handles the UART0 interrupt. It checks if a byte has been
+ * received via UART0 using the `check_receive` function. If a byte is received,
+ * it toggles the BLUE LED by inverting the state of the corresponding GPIO pin.
+ */
+void LPUART0_IRQHandler();
+
+/*******************************************************************************
+* Main function
+*******************************************************************************/
+int main () {
+	initUART0();
+	initLED();
+	SysTick_Config(SystemCoreClock / 4); /* 250ms */
+
+	while(1){
+
+	}
+}
+
+/*******************************************************************************
+* Function
+*******************************************************************************/
 void SysTick_Handler() {
 	if(countSystick == 4) {
 		UART0_SendString((uint8_t *)"Hello from MKE16Z!\n");
@@ -17,12 +61,7 @@ void LPUART0_IRQHandler() {
 		FGPIOD->PTOR |= 1 << BLUE_LED_PIN;
 	}
 }
-int main () {
-	initUART0();
-	initLED();
-	SysTick_Config(SystemCoreClock / 4); /* 250ms */
 
-	while(1){
-
-	}
-}
+/*******************************************************************************
+* EOF
+*******************************************************************************/
